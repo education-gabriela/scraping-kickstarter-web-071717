76a5a25257cde8bda16accadebe520a2b85d1e86
gabidavila <gabidavila@gmail.com>
2017-07-23 11:45:16 -0400

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,24 @@
-# require libraries/modules here
+require "nokogiri"
+require "pry"
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  projects = {:projects => {}}
+
+  kickstarter = Nokogiri::HTML(html)
+  list = kickstarter.css("#projects_list .project")
+
+  list.each do |item|
+    item_hash = {
+      item.css("h2 a").text => {
+        :image_link => item.css(".project-thumbnail img").attribute("src").value,
+        :description => item.css("p.bbcard_blurb").text.delete("\n"),
+        :location => item.css(".location-name").text,
+        :percent_funded => item.css(".funded strong").text.to_i
+      }
+    }
+    projects[:projects].merge!(item_hash)
+  end
+
+  projects[:projects]
 end
